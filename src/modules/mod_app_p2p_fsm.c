7f7b808e62c9d8e5c8dac9845e38ea502e94b2cb
e7 <jackzxty@126.com>
2016-11-14 15:51:44 +0800

    注销接口增加断开连接

--- src/modules/mod_app_p2p_fsm.c
+++ src/modules/mod_app_p2p_fsm.c
@@ -771,6 +771,14 @@ static void p2p_fsm_service(lts_socket_t *s)
             break;
         }
 
+        do {
+            // 断开连接
+            uintptr_t expire_time = 0;
+
+            while (-1 == lts_timer_reset(&lts_timer_heap,
+                                         &ts->conn->timer_node,
+                                         expire_time++));
+        } while (0);
         lts_rbmap_safe_del(&s_ts_set, &ts->map_node);
         free_ts_instance(ts);
 
